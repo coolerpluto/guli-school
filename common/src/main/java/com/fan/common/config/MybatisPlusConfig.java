c939e3d9d352179b1c52c2539c31ecfa8971fc1e
fby <839035008@qq.com>
2024-01-26 09:19:22 +0800

    mybatisplus配置类忘记添加@bean注解，导致分页失败

--- common/src/main/java/com/fan/common/config/MybatisPlusConfig.java
+++ common/src/main/java/com/fan/common/config/MybatisPlusConfig.java
@@ -5,11 +5,13 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.fan")
+@MapperScan("com.fan.edu_service.mapper")
 public class MybatisPlusConfig {
+    @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
